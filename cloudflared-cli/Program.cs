6d2eda970853b563a0bdd3ef6789b7b6f2d59be6
unknown <lungrbackup@protonmail.com>
2023-02-14 12:29:45 -0500

    fds

--- cloudflared-cli/Program.cs
+++ cloudflared-cli/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
-        public static void main(string[] args)
+        public static void Main(string[] args)
         {
-
+            Console.WriteLine("Cloudflared-CLI 0.0.1 \nWrote by ririinsta");
+            Console.Write("Enter hostname > ");
+            string ipthing = Console.ReadLine();
+            Console.Write("Enter local address > ");
+            string local = Console.ReadLine();
+            Console.WriteLine("C:\\Users\\26lungr\\Downloads\\cloudflared.exe access tcp --hostname " + ipthing + " --url " + local);
+        }
+        public static void checkfirst()
+        {
+            //Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile)
         }
     }
 }
